8d967495b590841a8854391a7a5507156fbeb614
Nico <nbl0357@gmail.com>
2021-07-07 00:47:54 -0400

    updated 100bandz.py
    Added to_async() function
    Added getmarketdata_embed()
    Start command is now asynchronous

--- 100bandz.py
+++ 100bandz.py
@@ -1,25 +1,23 @@
 import asyncio
 import discord
 from discord import colour
+from discord import message
 from discord.embeds import Embed
-from discord.ext import commands
-from discord.ext.commands.bot import BotBase
+from discord.ext import commands, tasks
 import yfinance as yf
 import pandas as pd
 import numpy as np
 import time
 from asyncio.tasks import Task
-from typing import AnyStr
 import random
 from discord import channel
-from discord.ext import commands, tasks
 import logging
 import os
 from itertools import cycle
 import json
 from datetime import datetime
 import sys
-
+import functools 
 
 logger = logging.getLogger('discord')   ##Error Logs are written to a file called discord.log
 logger.setLevel(logging.DEBUG) 
@@ -34,6 +32,15 @@ status = cycle(['AHHH','（。＾▽＾）','🤑🤑🤑','boolin'])
 
 stockdict = {"gme": "GME", 'tesla': 'TSLA', "arkk": "ARKK", "aqn": "AQN", 'suncor' : 'SU.TO', "bitcoin": "BTC-USD", 'arkg': 'ARKG', 'arkx': 'ARKX'}
 
+def to_async(syncfunc):
+    @functools.wraps(syncfunc)
+    async def sync_wrapper(*args, **kwargs):
+        loop = asyncio.get_event_loop()
+        func = functools.partial(syncfunc, *args, **kwargs)
+        return await loop.run_in_executor(None, func)
+
+    return sync_wrapper
+
 
 @bot.event
 async def on_ready():
@@ -41,31 +48,35 @@ async def on_ready():
     # await bot.change_presence(status=discord.Status.online, activity=discord.Game("🤑🤑🤑"),afk=False)
     print("We have logged in as {0.user}".format(bot))
         # First get the channel where the message should be sent
-    channel = discord.utils.get(bot.get_all_channels(), name='general')
-    embed=discord.Embed(title="Bot is Online!", color= 0x03fcf0)
+    channel = discord.utils.get(bot.get_all_channels(), name='bot-spam')
+    embed=discord.Embed(title="Bot is Online", color= 0x03fcf0)
     embed.set_author(name= "Get Your Bands Up", icon_url=bot.user.avatar_url)
     await channel.send(embed=embed)
 
+
+@to_async
+def getmarketdata_embed():
+    print("working :D")
+    embed=discord.Embed(title="shmoney 🙏", description= 'The Bread comes first', color= discord.Colour.random())
+    for kname, vtick in stockdict.items():
+        stock_ticker = vtick
+        stock_name = yf.Ticker(stock_ticker)
+        marketprice = str(stock_name.info.get("regularMarketPrice", None))
+        embed.add_field(name= stock_name.info.get("shortName"), value= '$'+ marketprice, inline=False)
+    return embed
+
+
 @bot.command()
 async def start(ctx):
-    starttime = time.time()
-    while True:
-        print("working :D")    
-        embed=discord.Embed(title="Getting your stocks ready! :)", description= 'Please wait...', color= 0x03fcf0)
-        embed.set_author(name= "Get Your Bands Up", icon_url=bot.user.avatar_url)
-        await ctx.send(embed=embed)
-        clr = "%06x" % random.randint(0, 0xFFFFFF)
-        embed=discord.Embed(title="shmoney 🙏", description= 'The Bread comes first', color= 0x03fcf0)
-        embed.set_author(name= ctx.author.display_name, icon_url= ctx.author.avatar_url)
-        for kname, vtick in stockdict.items():
-            stock_ticker = vtick
-            stock_name = yf.Ticker(stock_ticker)
-            marketprice = str(stock_name.info.get("regularMarketPrice", None))
-            embed.add_field(name= stock_name.info.get("shortName"), value= '$'+ marketprice, inline=False)
-            embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/838242488598134796/862063340707643452/1624112952498.jpg')
+    # while True:
 
+    embed=discord.Embed(title="Getting your stocks ready! :)", description= 'Please wait...', color= 0x03fcf0)
+    embed.set_author(name= "Get Your Bands Up", icon_url=bot.user.avatar_url)
+    message = await ctx.send(embed=embed)
+
+    while True:
+        embed = await getmarketdata_embed()
         await ctx.send(embed=embed)
-        await asyncio.sleep(120)
 
 
 @tasks.loop(minutes= 5)
